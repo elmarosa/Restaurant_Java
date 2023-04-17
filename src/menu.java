9d92dc23d0dcd693489d0223ff7eb1d535cc41f4
elmarosa <elmayuliarosa@gmail.com>
2023-04-17 09:16:36 +0700

    pilihan menu

---
+++ src/menu.java
@@ -0,0 +1,29 @@
+public class Menu {
+    private String nama;
+    private long harga;
+
+    public Menu(){
+        //Default constructor
+    }
+
+    public Menu(String nama, long harga) {
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public long getHarga() {
+        return harga;
+    }
+
+    public void setHarga(long harga) {
+        this.harga = harga;
+    }
+}
